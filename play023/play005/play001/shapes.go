297472820533a5065778761f88198ceec412f3bc
liguoqinjim <liguoqinjim23@gmail.com>
2018-04-29 18:50:12 +0800

    play001

--- play023/play005/play001/shapes.go
+++ play023/play005/play001/shapes.go
@@ -1,8 +1,22 @@
 package shapes
 
+import "math"
+
 type RectAngle struct {
 	Width  float64
 	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (r RectAngle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
 
 func Perimeter(rectangle RectAngle) float64 {
--- play023/play005/play001/shapes_test.go
+++ play023/play005/play001/shapes_test.go
@@ -13,11 +13,22 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := RectAngle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := RectAngle{12.0, 6.0}
+		got := rectangle.Area()
+		want := 72.0
 
-	if got != want {
-		t.Errorf("got %0.2f want %0.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %0.2f want %0.2f", got, want)
+		}
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.16
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
 }
