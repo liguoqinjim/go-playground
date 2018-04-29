feb76c1c58d7e88ee1754ae6dd66ae6422785e8b
liguoqinjim <liguoqinjim23@gmail.com>
2018-04-29 15:10:21 +0800

    sum

--- play023/play004/README.md
+++ play023/play004/README.md
@@ -1,5 +1,8 @@
-###
+### Arrays and slices
 
 |实验|简介|
 |---|---|
 |play001|array|
+
+#### 注意点
+ - go test -cover
---
+++ play023/play004/play002/sum.go
@@ -0,0 +1,11 @@
+package sum
+
+func Sum(nums []int) int {
+	sum := 0
+
+	for _, n := range nums {
+		sum += n
+	}
+
+	return sum
+}
