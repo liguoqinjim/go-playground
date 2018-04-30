a76cdd1c965436880d4dc99794a48eb38ab7ea3a
liguoqinjim <liguoqinjim23@gmail.com>
2018-04-30 20:52:43 +0800

    play008

--- play023/play008/countdown.go
+++ play023/play008/countdown.go
@@ -7,16 +7,28 @@ import (
 	"time"
 )
 
+type Sleeper interface {
+	Sleep()
+}
+
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		time.Sleep(1 * time.Second)
 		fmt.Fprintln(out, i)
 	}
 
 	fmt.Fprint(out, finalWord)
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
 }
 
 func main() {
