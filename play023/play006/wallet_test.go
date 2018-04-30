ef4e69d1212d1f129440e087984dbe31b2856b3a
liguoqinjim <liguoqinjim23@gmail.com>
2018-04-30 12:19:20 +0800

    play006

--- play023/play006/wallet_test.go
+++ play023/play006/wallet_test.go
@@ -3,17 +3,21 @@ package wallet
 import "testing"
 
 func TestWallet(t *testing.T) {
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
 		wallet.Deposit(Bitcoin(10))
 
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
@@ -21,11 +25,6 @@ func TestWallet(t *testing.T) {
 
 		wallet.Withdraw(10)
 
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 }
