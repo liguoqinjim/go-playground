be1133129e14e261480a2af1d7477e7bc21077ed
liguoqinjim <liguoqinjim23@gmail.com>
2018-04-30 12:51:05 +0800

    play006

--- play023/play006/README.md
+++ play023/play006/README.md
@@ -1,5 +1,6 @@
-###
+### Pointers & errors
 
 #### 注意点
  - Remember to only do enough to make the tests run. We need to make sure our test fails correctly with a clear error message.
  也就是说，我们一开始只要测试用例可以build过就可以了。不需要去关注这个测试结果正确与否。
+ - `https://github.com/kisielk/errcheck`，这个工具可以检查，我们是否有漏掉的对error的检查。
--- play023/play006/wallet_test.go
+++ play023/play006/wallet_test.go
@@ -3,25 +3,6 @@ package wallet
 import "testing"
 
 func TestWallet(t *testing.T) {
-	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
-		t.Helper()
-		got := wallet.Balance()
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	}
-
-	assertError := func(t *testing.T, got error, want string) {
-		if got == nil {
-			t.Fatal("wanted an error but didnt get one")
-		}
-
-		if got.Error() != want {
-			t.Errorf("got '%s',want '%s'", got, want)
-		}
-	}
-
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
@@ -33,9 +14,10 @@ func TestWallet(t *testing.T) {
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 
-		wallet.Withdraw(10)
+		err := wallet.Withdraw(10)
 
 		assertBalance(t, wallet, Bitcoin(10))
+		assertNoError(t, err)
 	})
 
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
@@ -44,6 +26,31 @@ func TestWallet(t *testing.T) {
 		err := wallet.Withdraw(Bitcoin(100))
 
 		assertBalance(t, wallet, startingBalance)
-		assertError(t, err, "cannot withdraw, insufficient funds")
+		assertError(t, err, ErrInsufficientFunds)
 	})
+}
+
+func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertError(t *testing.T, got, want error) {
+	if got == nil {
+		t.Fatal("wanted an error but didnt get one")
+	}
+
+	if got != want {
+		t.Errorf("got '%s',want '%s'", got, want)
+	}
+}
+
+func assertNoError(t *testing.T, got error) {
+	if got != nil {
+		t.Fatal("got an error but didnt want one")
+	}
 }
